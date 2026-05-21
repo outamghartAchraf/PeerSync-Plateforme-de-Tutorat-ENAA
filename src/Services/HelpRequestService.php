917d00d11c55b16586f3b458d406cd02669d90cc
outamghartAchraf <outamghart2000@gmail.com>
2026-05-21 11:38:33 +0100

    add function createRequest in service file

--- src/Services/HelpRequestService.php
+++ src/Services/HelpRequestService.php
@@ -3,6 +3,8 @@
 namespace Src\Services;
 
 require_once __DIR__ . '/../Repositories/HelpRequestRepository.php';
+include_once __DIR__ . '/../Entities/HelpRequest.php';
+
 
 use Src\Repositories\HelpRequestRepository;
 
@@ -28,6 +30,25 @@ class HelpRequestService
     public function countResolved(): int
     {
         return $this->repo->countByStatus('resolved');
+    }
+
+        public function createRequest(HelpRequest $helpRequest): bool
+    {
+        $title = trim($helpRequest->getTitle());
+        $description = trim($helpRequest->getDescription());
+        $technology = trim($helpRequest->getTechnology());
+        $creatorId = $helpRequest->getStudentId();
+
+        if (empty($title) || empty($description) || empty($technology)) {
+            throw new \Exception('Title, Description, and Technology are required');
+        }
+
+        $helpRequest->setTitle($title);
+        $helpRequest->setDescription($description);
+        $helpRequest->setTechnology($technology);
+        $helpRequest->setStatus('pending');
+
+        return $this->repo->create($helpRequest);
     }
 
 
