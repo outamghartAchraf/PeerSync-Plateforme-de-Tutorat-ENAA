de96482f52a236d61838ed993891b15e678a508a
outamghartAchraf <outamghart2000@gmail.com>
2026-05-22 10:42:51 +0100

    add function getUserSkills for resturn sills of user

--- src/Repositories/SkillRepository.php
+++ src/Repositories/SkillRepository.php
@@ -28,5 +28,24 @@ class SkillRepository
         return $statement->fetchAll(PDO::FETCH_OBJ);
     }
 
+    public function getUserSkills(int $userId, string $type): array
+    {
+        $statement = $this->pdo->prepare("
+            SELECT skills.*
+            FROM skills
+            INNER JOIN user_skill
+            ON skills.id = user_skill.skill_id
+            WHERE user_skill.user_id = :user_id
+            AND user_skill.type = :type
+        ");
+
+        $statement->execute([
+            'user_id' => $userId,
+            'type' => $type
+        ]);
+
+        return $statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
     
 }
