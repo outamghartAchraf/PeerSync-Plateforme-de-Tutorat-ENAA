733390bdcd92933741cbafca9b9ee8dfd7a0c560
outamghartAchraf <outamghart2000@gmail.com>
2026-05-22 11:25:46 +0100

    add code for delete skill

---
+++ src/Views/student/skill/delete.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['user'])) {
+    header("Location: ../../auth/login.php");
+    exit;
+}
+
+require_once __DIR__ . '/../../../Services/SkillService.php';
+
+use Src\Services\SkillService;
+
+$user = $_SESSION['user'];
+
+if (is_array($user)) {
+    $user = (object) $user;
+}
+
+$skillId = (int) $_GET['skill_id'];
+
+$type = $_GET['type'];
+
+$service = new SkillService();
+
+$service->deleteSkill(
+    $user->id,
+    $skillId,
+    $type
+);
+
+header("Location: list.php");
+exit;
