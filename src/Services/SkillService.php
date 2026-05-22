ba07aec01f8d1d63b9b66d9c39936f8a4deb5fa3
outamghartAchraf <outamghart2000@gmail.com>
2026-05-22 10:48:57 +0100

    add function getAllskill

---
+++ src/Services/SkillService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Src\Services;
+
+require_once __DIR__ . '/../Repositories/SkillRepository.php';
+
+use Src\Repositories\SkillRepository;
+
+class SkillService
+{
+    private SkillRepository $repository;
+
+    public function __construct()
+    {
+        $this->repository = new SkillRepository();
+    }
+
+    public function getAllSkills(): array
+    {
+        return $this->repository->getAllSkills();
+    }
+}
